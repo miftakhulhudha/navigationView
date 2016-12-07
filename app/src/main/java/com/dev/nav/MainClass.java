9aa3baaa1f89011c99179be75746817620c956a5
Miftakhul Hudha <hudha.miftakhulhudha@gmail.com>
2016-12-07 15:41:29 +0700

    first commit

---
+++ app/src/main/java/com/dev/nav/MainClass.java
@@ -0,0 +1,18 @@
+package com.dev.nav;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by miftakhulhudhadev on 07/12/16.
+ */
+
+public class MainClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
